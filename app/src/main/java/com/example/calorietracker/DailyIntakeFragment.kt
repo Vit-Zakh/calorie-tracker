6be04730d3b016533bc02040bdbcf15f1315ee95
vzakharenko <vit.zakharenko@gmail.com>
2021-01-22 16:36:51 +0200

    fixes according to comments

--- app/src/main/java/com/example/calorietracker/DailyIntakeFragment.kt
+++ app/src/main/java/com/example/calorietracker/DailyIntakeFragment.kt
@@ -1,21 +1,12 @@
 package com.example.calorietracker
 
 import android.os.Bundle
-import android.os.Handler
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DailyIntakeFragment : Fragment(R.layout.fragment_daily_intake) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
