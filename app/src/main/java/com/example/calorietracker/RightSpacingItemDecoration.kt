e8cb30b35620f290520d0de3531dcc180f719e34
vzakharenko <vit.zakharenko@gmail.com>
2021-01-29 09:44:07 +0200

    line formatting

--- app/src/main/java/com/example/calorietracker/RightSpacingItemDecoration.kt
+++ app/src/main/java/com/example/calorietracker/RightSpacingItemDecoration.kt
@@ -6,7 +6,12 @@ import androidx.recyclerview.widget.RecyclerView
 
 class RightSpacingItemDecoration : RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
         super.getItemOffsets(outRect, view, parent, state)
         when (parent.getChildLayoutPosition(view)) {
             0 -> outRect.left = 38
