324a6a9799c39142482fc52d35026a8059a00b45
vit_zakharenko <vit.zakharenko@gmail.com>
2021-02-07 00:20:21 +0200

    user and category name data binding

--- app/src/main/java/com/example/calorietracker/dailyintake/DailyIntakeAdapter.kt
+++ app/src/main/java/com/example/calorietracker/dailyintake/DailyIntakeAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.calorietracker.dailyintake
 
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +11,6 @@ import com.example.calorietracker.data.RecyclerData.*
 import com.example.calorietracker.databinding.LayoutMealItemBinding
 import com.example.calorietracker.databinding.LayoutTextItemBinding
 import com.example.calorietracker.databinding.LayoutUserItemBinding
-import com.example.calorietracker.extensions.loadImageByUrl
 import java.lang.RuntimeException
 
 class DailyIntakeAdapter() :
@@ -62,66 +60,33 @@ class DailyIntakeAdapter() :
         }
     }
 
-    override fun onBindViewHolder(
-        holder: RecyclerView.ViewHolder,
-        position: Int,
-        payloads: MutableList<Any>
-    ) {
-        if (payloads.isEmpty()) {
-            onBindViewHolder(holder, position)
-        } else {
-            (holder as UserViewHolder).bindWithPayloads((payloads[0] as Bundle))
-        }
-    }
+//    override fun onBindViewHolder(
+//        holder: RecyclerView.ViewHolder,
+//        position: Int,
+//        payloads: MutableList<Any>
+//    ) {
+//        if (payloads.isEmpty()) {
+//            onBindViewHolder(holder, position)
+//        } else {
+//            (holder as UserViewHolder).bindWithPayloads((payloads[0] as Bundle))
+//        }
+//    }
 
     class MealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-//        val mealBinding = LayoutMealItemBinding.bind(itemView)
-
-        val mMealBinding = LayoutMealItemBinding.bind(itemView)
+        private val mealBinding = LayoutMealItemBinding.bind(itemView)
 
         fun bind(meal: Meal) {
-            mMealBinding.meal = meal
+            mealBinding.meal = meal
         }
-
-//        fun bind(meal: Meal) {
-//            with(mealBinding) {
-//                mealTitle.text = meal.mealName
-//                mealCalories.text = meal.getIntakeCaloriesRounded()
-//                mealSize.text = meal.getConvertedWeight()
-//                mealImage.loadImageByUrl(meal.imageUrl)
-//            }
-//        }
     }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val userBinding = LayoutUserItemBinding.bind(itemView)
-        val res = itemView.resources
 
         fun bind(user: User) {
-
-            with(userBinding) {
-                userName.text = user.userName
-                userWeight.text = res.getString(R.string.user_weight_text, user.userWeight)
-                userDailyCalories.text =
-                    res.getString(
-                        R.string.user_daily_calories_text,
-                        "%.${2}f".format(user.userIntake)
-                    )
-                userImage.loadImageByUrl(user.userImage)
-            }
-        }
-
-        fun bindWithPayloads(bundle: Bundle) {
-
-            with(userBinding) {
-                userDailyCalories.text =
-                    res.getString(
-                        R.string.user_daily_calories_text,
-                        "%.${2}f".format(bundle.getDouble("dailyIntake"))
-                    )
-            }
+            userBinding.user = user
         }
     }
 
@@ -130,9 +95,7 @@ class DailyIntakeAdapter() :
         private val textBinding = LayoutTextItemBinding.bind(itemView)
 
         fun bind(string: String) {
-            with(textBinding) {
-                textToHold.text = string
-            }
+            textBinding.categoryName = string
         }
     }
 
--- app/src/main/java/com/example/calorietracker/extensions/ImageView.ext.kt
+++ app/src/main/java/com/example/calorietracker/extensions/ImageView.ext.kt
@@ -1,9 +1,11 @@
 package com.example.calorietracker.extensions
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.calorietracker.R
+import com.example.calorietracker.data.RecyclerData
 
 @BindingAdapter("loadImage")
 fun ImageView.loadImageByUrl(url: String?) {
@@ -13,4 +15,17 @@ fun ImageView.loadImageByUrl(url: String?) {
         .placeholder(R.drawable.ic_launcher_background)
         .error(R.drawable.ic_launcher_background)
         .into(this)
+}
+
+@BindingAdapter("userFormattedWeight")
+fun formatWeight(textView: TextView, user: RecyclerData.User) {
+    textView.text = textView.resources.getString(R.string.user_weight_text, user.userWeight)
+}
+
+@BindingAdapter("formattedCalories")
+fun formatCalories(textView: TextView, user: RecyclerData.User) {
+    textView.text = textView.resources.getString(
+        R.string.user_daily_calories_text,
+        "%.${2}f".format(user.userIntake)
+    )
 }
