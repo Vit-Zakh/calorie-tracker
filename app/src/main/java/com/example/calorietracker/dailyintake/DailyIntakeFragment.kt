df2c251a748ff29a30c746945a69fbd4a2814706
vit_zakharenko <vit.zakharenko@gmail.com>
2021-03-05 16:20:50 +0200

    wip

--- app/src/main/java/com/example/calorietracker/dailyintake/DailyIntakeFragment.kt
+++ app/src/main/java/com/example/calorietracker/dailyintake/DailyIntakeFragment.kt
@@ -12,7 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.calorietracker.R
 import com.example.calorietracker.databinding.FragmentDailyIntakeBinding
 import com.example.calorietracker.models.ui.DailyIntakeProps
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_daily_intake.*
 import kotlin.random.Random
 
 @AndroidEntryPoint
@@ -55,6 +57,25 @@ class DailyIntakeFragment : Fragment(R.layout.fragment_daily_intake) {
         }
 
         /** End of test buttons block */
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.intakeNavigationView)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        binding.bottomAppBar.setNavigationOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        binding.intakeNavigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.toProfile -> findNavController().navigate(
+                    DailyIntakeFragmentDirections.actionDailyIntakeFragmentToUserProfileFragment()
+                )
+                R.id.toDailtyIntake -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+//            menuItem.isChecked = true
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            true
+        }
 
         binding.floatingActionButton.setOnClickListener {
             openFoodList()
--- app/src/main/java/com/example/calorietracker/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/example/calorietracker/userprofile/UserProfileFragment.kt
@@ -1,21 +1,50 @@
 package com.example.calorietracker.userprofile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.calorietracker.R
+import com.example.calorietracker.databinding.FragmentUserProfileBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class UserProfileFragment : Fragment() {
+
+    private var fragmentBinding: FragmentUserProfileBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        fragmentBinding = binding
 
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.profileNavigationView)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        binding.profileBottomAppBar.setNavigationOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        binding.profileNavigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.toProfile -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                R.id.toDailtyIntake -> findNavController().navigate(
+                    UserProfileFragmentDirections.actionUserProfileFragmentToDailyIntakeFragment()
+                )
+            }
+//            menuItem.isChecked = true
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            true
+        }
+
+        return binding.root
+    }
 }
