41e37669843a54e637f41f6572149a30d6aa59b8
vit_zakharenko <vit.zakharenko@gmail.com>
2021-03-15 16:08:34 +0200

    tool bar manager

--- app/src/main/java/com/example/calorietracker/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/example/calorietracker/userprofile/UserProfileFragment.kt
@@ -1,12 +1,9 @@
 package com.example.calorietracker.userprofile
 
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +11,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.calorietracker.R
 import com.example.calorietracker.databinding.FragmentUserProfileBinding
 import com.example.calorietracker.models.ui.DailyIntakeProps
+import com.example.calorietracker.utils.ToolBarManager
 import com.example.calorietracker.utils.loadImageByUrl
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,13 +21,15 @@ class UserProfileFragment : Fragment() {
 
     private var fragmentBinding: FragmentUserProfileBinding? = null
     private val viewModel: UserProfileViewModel by viewModels()
+    private lateinit var toolBarManager: ToolBarManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        makeToolBarTransparent()
+        toolBarManager = ToolBarManager(requireActivity())
+        toolBarManager.makeToolBarTransparent()
 
         val binding = FragmentUserProfileBinding.inflate(inflater, container, false)
         fragmentBinding = binding
@@ -52,7 +52,8 @@ class UserProfileFragment : Fragment() {
             when (menuItem.itemId) {
                 R.id.toProfile -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                 R.id.toDailtyIntake -> {
-                    makeToolBarVisible()
+//                    makeToolBarVisible()
+                    toolBarManager.makeToolBarVisible()
                     findNavController().navigate(
                         UserProfileFragmentDirections.actionUserProfileFragmentToDailyIntakeFragment()
                     )
@@ -70,8 +71,6 @@ class UserProfileFragment : Fragment() {
         }
 
         binding.topAppBar.overflowIcon = resources.getDrawable(R.drawable.ic_settings_24)
-//        binding.topAppBar.
-
         return binding.root
     }
 
@@ -91,38 +90,8 @@ class UserProfileFragment : Fragment() {
         }
     }
 
-    private fun makeToolBarTransparent() {
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-            requireActivity().window.statusBarColor = Color.TRANSPARENT
-        }
-    }
-
-    private fun makeToolBarVisible() {
-        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        requireActivity().window.decorView.systemUiVisibility = View.VISIBLE
-        requireActivity().window.statusBarColor = resources.getColor(R.color.primary_back)
-    }
-
-    private fun setWindowFlag(bits: Int, on: Boolean) {
-        val win = requireActivity().window
-        val winParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or bits
-        } else {
-            winParams.flags = winParams.flags and bits.inv()
-        }
-        win.attributes = winParams
-    }
-
     override fun onDetach() {
         super.onDetach()
-        makeToolBarVisible()
+        toolBarManager.makeToolBarVisible()
     }
 }
